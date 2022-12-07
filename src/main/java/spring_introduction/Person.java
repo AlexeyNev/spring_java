79bb88f09e9a09e0e341a3a2668094e84ac89477
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-12-07 18:10:34 +0400

    spring_start2

---
+++ src/main/java/spring_introduction/Person.java
@@ -0,0 +1,14 @@
+package spring_introduction;
+
+public class Person {
+    private Pet pet;
+
+    public Person(Pet pet) {
+        this.pet = pet;
+    }
+
+    public void callYouPet() {
+        System.out.println("Hello my lovely Pet!");
+        pet.say();
+    }
+}
