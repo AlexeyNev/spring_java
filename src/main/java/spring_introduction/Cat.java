986c18216fb37baa309e0727b4317045055ece2c
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-12-07 18:27:09 +0400

    Внедрение зависимостей

--- src/main/java/spring_introduction/Cat.java
+++ src/main/java/spring_introduction/Cat.java
@@ -1,9 +1,12 @@
 package spring_introduction;
 
 public class Cat implements Pet {
+    public Cat() {
+        System.out.println("Cat bean is created");
+    }
 
     @Override
     public void say() {
-        System.out.println("meow");
+        System.out.println("meow meow");
     }
 }
--- src/main/java/spring_introduction/Test3.java
+++ src/main/java/spring_introduction/Test3.java
@@ -4,13 +4,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test3 {
     public static void main(String[] args) {
-//        Pet pet = new Dog();
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext");
 
-        Pet pet = context.getBean("myPet", Pet.class);
-
-        Person person = new Person(pet);
+        Person person = context.getBean("myPerson", Person.class);
         person.callYouPet();
         context.close();
     }
