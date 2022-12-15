283f9a8d92dc4552e3f644930331e62b672800a8
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-12-15 12:28:50 +0400

    Pointcut. part2

--- src/main/java/aop/SchoolLibrary.java
+++ src/main/java/aop/SchoolLibrary.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class SchoolLibrary extends AbstractLibrary {
 
-    @Override
-    public void getBook() {
-        System.out.println("Мы берем книгу из SchoolLibrary");
-    }
+//    @Override
+//    public void getBook() {
+//        System.out.println("Мы берем книгу из SchoolLibrary");
+//    }
 }
--- src/main/java/aop/Test1.java
+++ src/main/java/aop/Test1.java
@@ -8,8 +8,14 @@ public class Test1 {
                 new AnnotationConfigApplicationContext(MyConfig.class);
 
         UniLibrary unilibrary = context.getBean("uniLibrary", UniLibrary.class);
+        Book book = context.getBean("book", Book.class);
+        unilibrary.getBook(book);
+
+
+
+//        unilibrary.getMagazine();
 //        unilibrary.getBook();
-        unilibrary.returnBook();
+//        unilibrary.returnBook();
 //        unilibrary.getMagazine();
 
 //        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
