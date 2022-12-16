fc38f85b9228413890f087f3aa8b76d703eabb35
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-12-16 18:16:27 +0400

    Advice тип @AutoReturning

---
+++ src/main/java/aop/aspects/UniversityLoginAspect.java
@@ -0,0 +1,24 @@
+package aop.aspects;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class UniversityLoginAspect {
+
+    @Before("execution(* getStudents())")
+    public void beforeGetStudentsLoggingAdvice() {
+        System.out.println("beforeGetStudentsLoggingAdvice: " +
+                "логируем получение списка студентов перед методом getStudents");
+    }
+
+    @AfterReturning("execution(* getStudents())")
+    public void afterReturningGetStudentsLoggingAdvice() {
+        System.out.println("afterReturningGetStudentsLoggingAdvice: " +
+                "логируем получение списка студентов после работы метода getStudents");
+    }
+}
